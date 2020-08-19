02ea57c5a8bb36f3408d692b4ba16d4662f950b7
SVKratzer <samuel.kratzer1894@gmail.com>
2020-08-19 12:52:05 -0400

    Render all race weighted chart

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import "./styles/index.scss";
 import { createMaleWeightedBubbleChart } from  './scripts/ethnicity_weighted_male';
+import { createFemaleWeightedBubbleChart } from './scripts/ethnicity_weighted_female';
+import { createAllWeightedBubbleChart } from './scripts/ethnicity_weighted_all';
 
+createAllWeightedBubbleChart();
 createMaleWeightedBubbleChart();
+createFemaleWeightedBubbleChart();
 
