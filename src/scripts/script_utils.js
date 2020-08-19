0f85787f7984bc9fdc2c160f112ac8537b4efee3
SVKratzer <samuel.kratzer1894@gmail.com>
2020-08-19 11:06:47 -0400

    Seperate necesssary methods

---
+++ src/scripts/script_utils.js
@@ -0,0 +1,61 @@
+export const scale = 1;
+
+export const tooltip = d3.select("body")
+  .append("div")
+  .attr("class", "tooltip")
+
+export const spaceCirclesEvenly = (circles) => {
+  circles.attr("cx", (d, i) => {
+    let extraMargin = 0
+    if (i !== 0) {
+      const cumulativeMargin = parseInt(circles._groups[0][i - 1].attributes.cx.value);
+      const prevRadius = parseInt(circles._groups[0][i - 1].attributes.r.value);
+      extraMargin = cumulativeMargin + prevRadius
+    }
+    return (Math.pow(parseInt(d.numIncarcerated), 0.5) * scale) + (extraMargin) + 25;
+  });
+}
+
+export const colorCodeCircles = (circles) => {
+  const baseValue = parseInt(circles._groups[0][0].__data__.numIncarcerated);
+  circles.attr("class", (d, i) => {
+    const diff = (parseInt(d.numIncarcerated) / baseValue);
+    if (diff >= 2) {
+      return "high-positive";
+    } else if (diff >= 1.5 && diff < 2) {
+      return "med-positive";
+    } else if (diff > 1 && diff < 1.5) {
+      return "low-positive";
+    } else if (diff >= .75 && diff < 1) {
+      return "low-negative";
+    } else if (diff >= .5 && diff < .75) {
+      return "med-negative";
+    } else if (diff < .5) {
+      return "high-negative";
+    }
+  });
+};
+
+export const addEthnicityTooltips = (selection) => {
+  selection.on("mouseover", function (d) {
+    return tooltip
+      .style("visibility", "visible")
+      .text(d.ethnicity + ": " + d.numIncarcerated);
+  })
+  .on("mousemove", function (d) {
+    return tooltip
+      .style("top", (d3.event.pageY - 10) + "px")
+      .style("left", (d3.event.pageX + 10) + "px")
+      .text(d.ethnicity + ": " + d.numIncarcerated);
+  })
+  .on("mouseout", function (d) {
+    return tooltip
+      .style("visibility", "hidden");
+  });
+};
+
+export const formatClassName = (name) => {
+  const downcased = name.toLowerCase();
+  const arr = downcased.split(" ");
+  return arr.join("-")
+}
