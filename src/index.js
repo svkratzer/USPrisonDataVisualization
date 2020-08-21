d4df0de34d1fcf80d86704df3b890dbde3d189fe
SVKratzer <samuel.kratzer1894@gmail.com>
2020-08-21 10:06:57 -0400

    Remove unecessary imports

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 import "./styles/index.scss";
-import { createMaleWeightedBubbleChart } from  './scripts/ethnicity_weighted_male';
-import { createFemaleWeightedBubbleChart } from './scripts/ethnicity_weighted_female';
 import { createAllWeightedBubbleChart } from './scripts/ethnicity_weighted_all';
 
 import {
