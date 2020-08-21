c8083d0a9959d7b3892ed8a3cd567b099ecb6d7d
SVKratzer <samuel.kratzer1894@gmail.com>
2020-08-21 09:59:04 -0400

    Update circle radii function

--- src/scripts/script_utils.js
+++ src/scripts/script_utils.js
@@ -4,17 +4,20 @@ export const tooltip = d3.select("body")
 
 export const spaceCirclesEvenly = (circles, scale) => {
   let baseRadius = 0;
+  let cumulativeMargin = 0;
+  const lexicalCircles = circles;
+
   circles.attr("cx", (d, i) => {
       let extraMargin = 0
       let ratio = 1;
       if (i === 0) { 
         baseRadius = Math.pow(parseInt(d.numIncarcerated), 0.5); 
       } else {
-        const cumulativeMargin = parseInt(circles._groups[0][i - 1].attributes.cx.value);
-        const prevRadius = parseInt(circles._groups[0][i - 1].attributes.r.value);
+        const prevRadius = parseInt(lexicalCircles._groups[0][i - 1].attributes.r.value);
         extraMargin = cumulativeMargin + prevRadius
         ratio = Math.pow(parseInt(d.numIncarcerated), 0.5) / baseRadius;
       }
+      cumulativeMargin = ((25 * scale * ratio) + (extraMargin) + 25);
       return (25 * scale * ratio) + (extraMargin) + 25;
     });
 }
@@ -40,27 +43,34 @@ export const colorCodeCircles = (circles) => {
 };
 
 export const addEthnicityTooltips = (selection) => {
-  selection.on("mouseover", function (d) {
+  selection.on("mouseover", (d) => {
     return tooltip
       .style("visibility", "visible")
-      .text(d.ethnicity + ": " + d.numIncarcerated);
+      .text(createTooltipText(d.ethnicity, d.numIncarcerated));
   })
-  .on("mousemove", function (d) {
+  .on("mousemove", (d) => {
     return tooltip
-      .style("top", (d3.event.pageY - 10) + "px")
-      .style("left", (d3.event.pageX + 10) + "px")
-      .text(d.ethnicity + ": " + d.numIncarcerated);
+      .style("top", (d3.event.pageY - 20) + "px")
+      .style("left", (d3.event.pageX + 20) + "px")
+      .text(createTooltipText(d.ethnicity, d.numIncarcerated));
   })
-  .on("mouseout", function (d) {
+  .on("mouseout", (d) => {
     return tooltip
       .style("visibility", "hidden");
   });
 };
 
+const createTooltipText = (ethnicity, numIncarcerated) => {
+  const numberWithCommas = (x) => {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
+  return `${ethnicity}: ${numberWithCommas(numIncarcerated)} per 100,000`
+}
+
 export const formatClassName = (name) => {
   const downcased = name.toLowerCase();
   const arr = downcased.split(" ");
-  return arr.join("-")
+  return arr[0];
 }
 
 export const scaleCircleRadii = (circles, scale) => {
@@ -76,7 +86,3 @@ export const scaleCircleRadii = (circles, scale) => {
       }
     });
 };
-
-export const spaceEthnicityWeightedMale = () => {
-
-}
